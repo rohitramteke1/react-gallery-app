fb1a915f1518c611ad6873bb119a141ec78717a8
Rohit Maroti Ramteke <ramteker284@gmail.com>
2023-12-01 18:48:37 +0530

    loader added

--- src/gallery/GalleryComponent.jsx
+++ src/gallery/GalleryComponent.jsx
@@ -2,46 +2,68 @@ import React, { useState } from "react";
 import api from "../api.json";
 import "./gallery.css";
 import ClearIcon from "@mui/icons-material/Clear";
-import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+// ... your existing imports ...
 
 const GalleryComponent = () => {
   const [modal, setModal] = useState(false);
   const [imgSrc, setImgSrc] = useState("");
   const [indexOfImage, setIndexOfImage] = useState(0);
+  const [loading, setLoading] = useState(false);
 
-  const openModal = (index, imageData) => {
+  const getImage = (images) => {
+    const { index, imageData } = images;
     setModal(true);
     setImgSrc(imageData.url);
     setIndexOfImage(index);
   };
 
-  const closeAndResetModal = () => {
-    setModal(false);
-    setImgSrc("");
-    setIndexOfImage(0);
+  const leftImageHandle = () => {
+    if (indexOfImage > 0) {
+      setLoading(true);
+      setTimeout(() => {
+        setIndexOfImage(indexOfImage - 1);
+        setImgSrc(api[indexOfImage - 1].url);
+        setLoading(false);
+      }, 500);
+    }
   };
 
-  const navigateImage = (direction) => {
-    const newIndex = indexOfImage + direction;
-    if (newIndex >= 0 && newIndex < api.length) {
-      setIndexOfImage(newIndex);
-      setImgSrc(api[newIndex].url);
+  const rightImageHandle = () => {
+    if (indexOfImage < api.length - 1) {
+      setLoading(true);
+      setTimeout(() => {
+        setIndexOfImage(indexOfImage + 1);
+        setImgSrc(api[indexOfImage + 1].url);
+        setLoading(false);
+      }, 500);
     }
   };
 
   return (
     <>
       <div className={modal ? "modal open" : "modal"}>
-        <ClearIcon className="close-icon" onClick={closeAndResetModal} />
-        <ArrowBackIosNewIcon className="left-icon" onClick={() => navigateImage(-1)} />
-        <ArrowForwardIosIcon className="right-icon" onClick={() => navigateImage(1)} />
+        <ClearIcon className="close-icon" onClick={() => setModal(false)} />
+        <ArrowBackIosIcon
+          className="left-icon"
+          onClick={() => leftImageHandle()}
+        />
+        <ArrowForwardIosIcon
+          className="right-icon"
+          onClick={() => rightImageHandle()}
+        />
+        {loading && <div className="loader"></div>}
         <img src={imgSrc} alt="error" />
       </div>
 
       <div className="gallery">
         {api.map((imageData, index) => (
-          <div key={index} className="pics" onClick={() => openModal(index, imageData)}>
+          <div
+            key={index}
+            className="pics"
+            onClick={() => getImage({ imageData, index })}
+          >
             <img src={imageData.url} alt="error" style={{ width: "100%" }} />
           </div>
         ))}
